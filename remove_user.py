4890e8a6f169a5e5b20ade2e15a22f58ad2bfc2d
Samir Rodriguez <samirr14@byu.edu>
2025-04-02 13:13:44 -0600

    removeUser

---
+++ remove_user.py
@@ -0,0 +1,103 @@
+from boxsdk import Client, OAuth2, BoxAPIException
+import threading
+import api_connect
+#GLOBAL VARIABLES
+ACCESS_TOKEN = ''
+REFRESH_TOKEN= ''
+DEV_TOKEN = "r1WKmG7Pdt8iRhZF9s3W0PCJ78JcXJWl"
+token_url = "https://api.box.com/oauth2/token"
+USE_TOKEN = ACCESS_TOKEN
+
+lock = threading.Lock()
+thread_base= 1
+
+
+
+def look_into_folders(client,file,removed_from, user_id,folder_id):
+    thread_name = threading.current_thread().name
+    log = f'[{thread_name}] started folder look up for folder: {client.folder(folder_id=folder_id).get().name} - {folder_id}\n'
+    file.write(log)
+    print(log)
+
+    print(f"[{thread_name}] API call get_collaborations\n")
+    file.write(f"[{thread_name}] API call get_collaborations\n")
+
+    collaborations = client.folder(folder_id = folder_id).get_collaborations()
+
+    print(f"[{thread_name}] API call get_collaborations result:{collaborations}\n")
+    file.write(f"[{thread_name}] API call get_collaborations result:{collaborations}\n") 
+    
+    collabs = [c.id for c in collaborations if c.accessible_by.id == user_id]
+
+    print(f'collabs: {collabs}')
+    file.write(f'collabs: {collabs}')
+    with lock:
+        for collab in collabs:
+            print(f'deleting collab: {collab}')
+            file.write(f'deleting collab: {collab}')
+            client.collaboration(collab).delete()
+            
+    
+    with lock:
+        print(f'[{thread_name}] API call get_items\n')
+        file.write(f'[{thread_name}] API call get_items\n')
+        
+        items = client.folder(folder_id=folder_id).get_items()
+   
+        print(f'[{thread_name}] API call get_items result: {items}\n')
+        file.write(f'[{thread_name}]API call get_items result: {items}\n')
+
+    #Recursive call look_into_folders from all the folders
+    for item in items:
+        type = item.type.capitalize()
+        if (type=="Folder"):
+            look_into_folders(client,file, removed_from,user_id, item.id)
+
+def main(access_token,user_id, folder_id,thread_base):
+    thread_base = thread_base
+
+    main_thread_name = 'Thread-Main'
+
+    ACCESS_TOKEN = access_token
+    USE_TOKEN = ACCESS_TOKEN
+
+
+    print("started box connection\n")
+    #Create Connection 
+    auth = OAuth2(client_id=None, client_secret=None, access_token=USE_TOKEN)
+    client = Client(auth)
+    print("[{main_thread_name}] API call get().name from user{user_id}\n")
+    
+
+    folder_name = client.folder(folder_id=folder_id).get().name
+    user_name = "test"
+    #Open Files to log
+    log_file_name= f'result/remove/log_del_u_{user_id}_f{folder_id}.txt'
+    removed_from_name= f'result/remove/del_u_{user_id}_f_{folder_id}.csv'
+
+    #Empty Files
+    open(log_file_name,"w").close()
+    open(removed_from_name,"w").close()
+
+    #open for appending
+    file=open(log_file_name, "a", encoding="utf-8") 
+    removed_from =open(removed_from_name, "a", encoding="utf-8") 
+    removed_from.write("thread,user,user_id,name,email,folder_id,folder_name,collab_id\n")
+
+    
+    print(f'box connection made: {client}\n')
+    file.write(f'box connection made: {client}\n\n')
+
+    
+    print(f"started folder look up:{folder_id}\n")
+    file.write(f"started folder look up:{folder_id}\n")
+    
+    #Make recursive call
+    look_into_folders(client,file,removed_from,user_id,folder_id)
+
+    file.close()
+    removed_from.close()
+
+
+#local test
+main(DEV_TOKEN, "28569229852","314803162828",0)
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import api_connect
-import main_threads
+import get_collabs
+import remove_user
 from boxsdk import BoxAPIException
 PORT = 5000
 app = Flask(__name__)
@@ -10,20 +11,36 @@ def callback():
     auth_code = request.args.get("code")  # Get the authorization code
     print(f"authcode: {auth_code}\n")
     try:
-
         access_token, refresh_token = api_connect.get_access_token(auth_code)
         print(f"access_token: {access_token}\nrefresh_token: {refresh_token}\n")
+        option = -1
+        while(option !=2):
+            option = input("Select an option [0-1-2]\n [0]Find Collabs\n [1]Remove User\n [2]Exit:")
+            print(f"you selected option {option}\n")
+            thread_base = 0
+            if (option =="0"):
+                folder_id = input("file id: ")
+                thread_base = input("Select an option [0/1]\n"+
+                                    "[0] No\n[1] Yes:\n")
 
-        folder_id = input("file id: ")
-        thread_base = input("thread?[1 - yes, 0 - no]: ")
+                print(f"You entered: {folder_id}")
+                print("Start running main_threads\n")
+                get_collabs.main(access_token, folder_id,thread_base)
+                
+                print("Finish running main_threads\n")
+            elif (option=="1"):
+                print(f"start typing...{option}\n")
+                user_id = input("user id: ")
+                folder_id = input("folder to remove from\n" +
+                                    "note: it removes them from all files in the tree:\n")
 
-        print(f"You entered: {folder_id}")
-        print("Start running main_threads\n")
-        main_threads.main(access_token, folder_id,thread_base)
+                print(f"removing user {user_id} from parent folder {folder_id}\n")
+                remove_user.main(access_token, user_id, folder_id, thread_base)
+                print(f"finished removing user {user_id}")
+            elif (option =="2"):
+                break
     except BoxAPIException as e:
         api_connect.handle_box_exception(open("exception file.txt","w"),e)
-    print("Finish running main_threads\n")
-
 
     return f"{auth_code}"
 
