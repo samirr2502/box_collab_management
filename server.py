3bfa15be71af2bf204e997b8f489e03fe7fb417f
Samir Rodriguez <samirr14@byu.edu>
2025-04-03 13:59:39 -0600

    created simple html

--- server.py
+++ server.py
@@ -1,15 +1,47 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+import requests
+import webbrowser
 import api_connect
 import get_collabs
 import remove_user
+
 from boxsdk import BoxAPIException
 PORT = 5000
 app = Flask(__name__)
 
 @app.route("/")
 def callback():
+    template = render_template('index.html')
+    return template
+
+
+@app.route("/get_auth")
+def get_auth():
+    CLIENT_ID = "020r4pyyewrt5si70y5mtvsg4g6kl3qq"
+    REDIRECT_URI = "http://127.0.0.1:5000/auth"
+    auth_code_url = f"https://account.box.com/api/oauth2/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}"
+    response = requests.get(auth_code_url)
+    webbrowser.open(auth_code_url)
+
+    #auth_code_response = response.json()
+
+    #auth_code_ = auth_code_response.get("code")
+
+    print(f"Response: {response}")
+    return "redirecting "
+
+
+
+@app.route("/auth")
+def auth():
     auth_code = request.args.get("code")  # Get the authorization code
     print(f"authcode: {auth_code}\n")
+    template = render_template("home.html")
+    
+    #Comment Return template and uncomment 'try' to test in the console
+    return template
+
+    """
     try:
         option = -1
         while(option != 2):
@@ -65,6 +97,6 @@ def callback():
     except Exception as e:
         print(f"Unexpected error during initial access token retrieval: {e}")
         return f"{auth_code}"
-
+    """
 if __name__ == "__main__":
     app.run(port=PORT)
